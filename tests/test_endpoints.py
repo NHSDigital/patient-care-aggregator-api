5156a7fdb345cd3e977fac8e2ab8a48497973d50
Steve Smith <274815664+stevesmith-nhs@users.noreply.github.com>
2026-06-12 14:14:39 +0100

    APM-000 chore: align local tooling and dependency setup (#150)
    * WPP-10528 chore: align local tooling and dependency setup
    
    Fixes broken dependency on deprecated api-test-utils package and updates local tooling setup to match latest versions used in other NHS APIs.
    
    - switch Python test utility install to pytest-nhsd-apim==5.0.14 to align with latest version used in other NHS APIs
    - make git hook setup executable and add install-hooks target
    - pin @openapitools/openapi-generator-cli to 2.32.0
    - refresh package-lock.json to current npm lockfile format and resolved deps
    - ignore generated openapitools.json
    - remove outdated pyproject.toml comment about old api-test-utils install path
    
    * WPP-10528 chore: align local tooling and dependency setup
    
    Removed the no-deps flag from Makefile:5, so CI/local now installs transitive dependencies for pytest-nhsd-apim.
    
    * Migrate endpoint tests to https://github.com/NHSDigital/pytest-nhsd-apim and smoke tests to local client/config helpers
    
    * WPP-10528 chore
    replace legacy Node install flow by switching Makefile:7 from npm install --legacy-peer-deps to npm ci for deterministic dependency resolution in pipelines
    
    * install now depends on install-hooks instead of pre-commit, so any future prerequisites added under install-hooks will be picked up automatically by make install.
    
    * update chmod
    
    * lint and update readme
    
    * update readme
    
    * undo hook related changes
    
    * undo hook related changes

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,35 +1,49 @@
-import pytest
-from api_test_utils.apigee_api_apps import ApigeeApiDeveloperApps
-from api_test_utils.apigee_api_products import ApigeeApiProducts
-import uuid
-from time import time
-import jwt
-import requests
-from .configuration import config
 import json
+import os
+from time import time
+import uuid
+
+import jwt
+import pytest
+import requests
+from pytest_nhsd_apim.apigee_apis import (
+    ApigeeClient,
+    ApigeeNonProdCredentials,
+    ApiProductsAPI,
+    DeveloperAppsAPI,
+)
 
 SESSION = requests.Session()
+DEFAULT_DEVELOPER = "apm-testing-internal-dev@nhs.net"
+
+
+def _config():
+    from .configuration import config
+
+    return config
 
 
 class TestEndpoints:
 
     @pytest.fixture()
-    async def test_app_and_product(self):
-        """Create a fresh test app and product consuming the patient-care-agregator-api proxy
-        The app and products are destroyed at the end of the test
-        """
+    def test_app_and_product(self):
+        """Create and clean up an app+product pair for the proxy-under-test."""
         print("\nCreating Default App and Product..")
-        apigee_product = ApigeeApiProducts()
-        await apigee_product.create_new_product()
-        await apigee_product.update_proxies(
-            [config.PROXY_NAME, f"identity-service-{config.ENVIRONMENT}"]
+        cfg = _config()
+
+        apigee_config = ApigeeNonProdCredentials(
+            apigee_access_token=os.environ["APIGEE_API_TOKEN"]
         )
-        await apigee_product.update_scopes(
-            ["urn:nhsd:apim:user-nhs-login:P9:patient-care-aggregator-api"]
-        )
-        # Product ratelimit
+        client = ApigeeClient(config=apigee_config)
+        products_api = ApiProductsAPI(client=client)
+        apps_api = DeveloperAppsAPI(client=client)
+
+        developer_email = os.environ.get("APIGEE_DEVELOPER", DEFAULT_DEVELOPER)
+        product_name = f"{cfg.PROXY_NAME}-e2e-{uuid.uuid4().hex[:8]}"
+        app_name = f"{cfg.PROXY_NAME}-e2e-app-{uuid.uuid4().hex[:8]}"
+
         product_ratelimit = {
-            f"{config.PROXY_NAME}": {
+            cfg.PROXY_NAME: {
                 "quota": {
                     "limit": "300",
                     "enabled": True,
@@ -39,102 +53,122 @@ class TestEndpoints:
                 "spikeArrest": {"ratelimit": "100ps", "enabled": True},
             }
         }
-        await apigee_product.update_attributes(
-            {"ratelimiting": json.dumps(product_ratelimit)}
-        )
 
-        await apigee_product.update_environments([config.ENVIRONMENT])
-
-        apigee_app = ApigeeApiDeveloperApps()
-        await apigee_app.create_new_app()
-
-        # Set default JWT Testing resource url and app ratelimit
-        app_ratelimit = {
-            f"{config.PROXY_NAME}": {
-                "quota": {
-                    "limit": "300",
-                    "enabled": True,
-                    "interval": 1,
-                    "timeunit": "minute",
-                },
-                "spikeArrest": {"ratelimit": "100ps", "enabled": True},
-            }
-        }
-        await apigee_app.set_custom_attributes(
+        product = products_api.post_products(
             {
-                "jwks-resource-url": "https://raw.githubusercontent.com/NHSDigital/"
-                "identity-service-jwks/main/jwks/internal-dev/"
-                "9baed6f4-1361-4a8e-8531-1f8426e3aba8.json",
-                "ratelimiting": json.dumps(app_ratelimit),
+                "name": product_name,
+                "displayName": product_name,
+                "approvalType": "auto",
+                "environments": [cfg.ENVIRONMENT],
+                "proxies": [cfg.PROXY_NAME, f"identity-service-{cfg.ENVIRONMENT}"],
+                "scopes": ["urn:nhsd:apim:user-nhs-login:P9:patient-care-aggregator-api"],
+                "attributes": [
+                    {"name": "access", "value": "private"},
+                    {"name": "ratelimiting", "value": json.dumps(product_ratelimit)},
+                ],
+                "quota": "300",
+                "quotaInterval": "1",
+                "quotaTimeUnit": "minute",
             }
         )
 
-        await apigee_app.add_api_product(api_products=[apigee_product.name])
+        app_ratelimit = {
+            cfg.PROXY_NAME: {
+                "quota": {
+                    "limit": "300",
+                    "enabled": True,
+                    "interval": 1,
+                    "timeunit": "minute",
+                },
+                "spikeArrest": {"ratelimit": "100ps", "enabled": True},
+            }
+        }
 
-        yield apigee_product, apigee_app
+        app = apps_api.create_app(
+            email=developer_email,
+            body={
+                "name": app_name,
+                "apiProducts": [product["name"]],
+                "callbackUrl": "https://google.com/callback",
+                "attributes": [
+                    {
+                        "name": "jwks-resource-url",
+                        "value": "https://raw.githubusercontent.com/NHSDigital/"
+                        "identity-service-jwks/main/jwks/internal-dev/"
+                        "9baed6f4-1361-4a8e-8531-1f8426e3aba8.json",
+                    },
+                    {"name": "ratelimiting", "value": json.dumps(app_ratelimit)},
+                ],
+            },
+        )
 
-        # Teardown
+        test_app = {
+            "client_id": app["credentials"][0]["consumerKey"],
+            "name": app["name"],
+            "developer": developer_email,
+        }
+
+        yield product, test_app
+
         print("\nDestroying Default App and Product..")
-        await apigee_app.destroy_app()
-        await apigee_product.destroy_product()
+        apps_api.delete_app_by_name(email=test_app["developer"], app_name=test_app["name"])
+        products_api.delete_product_by_name(product["name"])
 
     @pytest.fixture()
-    async def get_token(self, test_app_and_product):
-        test_product, test_app = test_app_and_product
+    def get_token(self, test_app_and_product):
+        _, test_app = test_app_and_product
+        cfg = _config()
 
-        """Call identity server to get an access token"""
         # Create and sign mock id_token
-        id_token_private_key = config.ENV["id_token_private_key"]
+        id_token_private_key = cfg.ENV["id_token_private_key"]
         with open(id_token_private_key, "r") as f:
             id_token_private_key = f.read()
-        headers = {
-            "typ": "JWT",
-            "alg": "RS512",
-            "kid": "nhs-login",
-        }
-        claims = {
-            "sub": "49f470a1-cc52-49b7-beba-0f9cec937c46",
-            "birthdate": "1968-02-12",
-            "nhs_number": "9912003072",  # you can change this nhs-number as required :)
-            "iss": "https://internal-dev.api.service.nhs.uk",
-            "vtm": "https://auth.sandpit.signin.nhs.uk/trustmark/auth.sandpit.signin.nhs.uk",
-            "aud": "some-client-id",
-            "id_status": "verified",
-            "token_use": "id",
-            "surname": "MILLAR",
-            "auth_time": 1623849201,
-            "vot": "P9.Cp.Cd",
-            "identity_proofing_level": "P9",
-            "exp": 4114224185,
-            "iat": 1623849271,
-            "family_name": "MILLAR",
-            "jti": "8edabe2b-c7ff-40bd-bc7f-0b8dc6a52423",
-        }
+
         id_token_jwt = jwt.encode(
-            claims, id_token_private_key, headers=headers, algorithm="RS512"
+            {
+                "sub": "49f470a1-cc52-49b7-beba-0f9cec937c46",
+                "birthdate": "1968-02-12",
+                "nhs_number": "9912003072",
+                "iss": "https://internal-dev.api.service.nhs.uk",
+                "vtm": "https://auth.sandpit.signin.nhs.uk/trustmark/auth.sandpit.signin.nhs.uk",
+                "aud": "some-client-id",
+                "id_status": "verified",
+                "token_use": "id",
+                "surname": "MILLAR",
+                "auth_time": 1623849201,
+                "vot": "P9.Cp.Cd",
+                "identity_proofing_level": "P9",
+                "exp": 4114224185,
+                "iat": 1623849271,
+                "family_name": "MILLAR",
+                "jti": "8edabe2b-c7ff-40bd-bc7f-0b8dc6a52423",
+            },
+            id_token_private_key,
+            headers={"typ": "JWT", "alg": "RS512", "kid": "nhs-login"},
+            algorithm="RS512",
         )
 
         # Create jwt for client assertion (APIM-authentication)
-        client_assertion_private_key = config.ENV["client_assertion_private_key"]
+        client_assertion_private_key = cfg.ENV["client_assertion_private_key"]
         with open(client_assertion_private_key, "r") as f:
             private_key = f.read()
-        url = "https://internal-dev.api.service.nhs.uk/oauth2/token"
-        claims = {
-            "sub": test_app.client_id,  # TODO:save this on secrets manager or create app on the fly
-            "iss": test_app.client_id,
-            "jti": str(uuid.uuid4()),
-            "aud": url,
-            "exp": int(time()) + 300,  # 5mins in the future
-        }
 
-        additional_headers = {"kid": "test-1"}
+        token_url = "https://internal-dev.api.service.nhs.uk/oauth2/token"
         client_assertion = jwt.encode(
-            claims, private_key, algorithm="RS512", headers=additional_headers
+            {
+                "sub": test_app["client_id"],
+                "iss": test_app["client_id"],
+                "jti": str(uuid.uuid4()),
+                "aud": token_url,
+                "exp": int(time()) + 300,
+            },
+            private_key,
+            algorithm="RS512",
+            headers={"kid": "test-1"},
         )
 
-        # Get token using token exchange
         resp = SESSION.post(
-            url,
+            token_url,
             headers={"foo": "bar"},
             data={
                 "grant_type": "urn:ietf:params:oauth:grant-type:token-exchange",
@@ -151,14 +185,10 @@ class TestEndpoints:
         return resp.json()["access_token"]
 
     def test_happy_path(self, get_token):
-        # Given I have a token
+        cfg = _config()
         token = get_token
-        expected_status_code = 200
-        proxy_url = (
-            f"https://internal-dev.api.service.nhs.uk/{config.ENV['base_path']}/status"
-        )
-        # When calling the proxy
-        headers = {"Authorization": f"Bearer {token}"}
-        resp = SESSION.get(url=proxy_url, headers=headers)
-        # Then
-        assert resp.status_code == expected_status_code
+        proxy_url = f"https://internal-dev.api.service.nhs.uk/{cfg.ENV['base_path']}/status"
+
+        resp = SESSION.get(url=proxy_url, headers={"Authorization": f"Bearer {token}"})
+
+        assert resp.status_code == 200
